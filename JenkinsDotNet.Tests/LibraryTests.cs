fb61d210db02f153b9d89df35db7ee7bb7e936c5
xuyijun <ayijun@gmail.com>
2016-09-29 16:33:38 +0800

    upgrade to dotnet core

---
+++ JenkinsDotNet.Tests/LibraryTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JenkinsDotNet.Tests
+{
+
+    [TestClass]
+    public class LibraryTests
+    {
+        [TestMethod]
+        public void TestExamples()
+        {
+            Assert.AreEqual(100, 100);
+        }
+
+    }
+}
